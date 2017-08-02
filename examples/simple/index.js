06e4613bfeec3016c17b21c2f996a79d45e6c398
Michal Szorád <miso.szorad@gmail.com>
2017-08-02 08:46:21 +0200

    Added AmazonLoginButton into Example

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import {FacebookLoginButton, GoogleLoginButton, GithubLoginButton, TwitterLoginButton} from '../../src';
+import {
+    FacebookLoginButton,
+    GoogleLoginButton,
+    GithubLoginButton,
+    TwitterLoginButton,
+    AmazonLoginButton
+} from '../../src';
 
 render(<div style={{width: 300, margin: '75px auto'}}>
     <FacebookLoginButton onClick={e => alert('Hello from facebook button')}/>
     <GoogleLoginButton/>
     <GithubLoginButton/>
     <TwitterLoginButton/>
+    <AmazonLoginButton/>
 </div>, document.getElementById('app'));
