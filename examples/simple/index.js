d39e1a5f9a929942b5846c06a2f638a026d7f6b3
Michal Szorád <miso.szorad@gmail.com>
2017-08-02 10:15:15 +0200

    Added visual tests into examples

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -9,6 +9,17 @@ import {
     AmazonLoginButton
 } from '../../src';
 
+/*
+
+// visual tests
+<FacebookLoginButton onClick={e => alert('Hello from facebook button')} iconSize="150px" size="150px"/>
+<GoogleLoginButton size="150px"/>
+<GithubLoginButton icon="github" iconFormat={name => `demo-icon icon-${name}-circled`}/>
+<TwitterLoginButton style={{color: 'red'}} activeStyle={{color: 'blue'}}/>
+<AmazonLoginButton icon="gplus"/>
+
+ */
+
 render(<div style={{width: 300, margin: '75px auto'}}>
     <FacebookLoginButton onClick={e => alert('Hello from facebook button')}/>
     <GoogleLoginButton/>
