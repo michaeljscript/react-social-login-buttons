3514c0f5eda3b309e693b8b93573dd8643236f76
Michal Szorád <miso.szorad@gmail.com>
2018-04-14 16:03:00 +0200

    Remove console.log

--- stories/index.js
+++ stories/index.js
@@ -2,8 +2,6 @@ import * as buttons from "../src/buttons";
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-console.log(Object.values(buttons));
-
 const renderAllButtons = buttons => <div>{Object.values(buttons).map((Button, i) => <Button key={i} />)}</div>;
 
 //eslint-ignore-next-line
