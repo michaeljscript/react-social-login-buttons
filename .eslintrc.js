0d6bc2a28b3167534465a720d765c9bc2c1a4e0b
Michal Szorád <miso.szorad@gmail.com>
2018-04-14 14:29:07 +0200

    Remove deprecated option

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
     env: { browser: true },
     extends: ["react"],
-    ecmaFeatures: {
-        jsx: true,
-        modules: true,
-    },
     rules: {
         "no-use-before-define": "off",
         "comma-dangle": [
