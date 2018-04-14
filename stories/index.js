b506369efcfb004a67ac46af58a0bb00dbce0836
Michal Szorád <miso.szorad@gmail.com>
2018-04-14 17:15:25 +0200

    Fix story title

--- stories/index.js
+++ stories/index.js
@@ -5,4 +5,4 @@ import { storiesOf } from "@storybook/react";
 const renderAllButtons = buttons => <div>{Object.values(buttons).map((Button, i) => <Button key={i} />)}</div>;
 
 //eslint-disable-next-line
-storiesOf("Button", module).add("All buttons", () => renderAllButtons(buttons));
+storiesOf("Social login buttons", module).add("All buttons", () => renderAllButtons(buttons));
