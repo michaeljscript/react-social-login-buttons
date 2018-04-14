b13d06625336782826cb3a694accf5e1223c216b
Michal Szorád <miso.szorad@gmail.com>
2018-04-14 23:07:31 +0200

    Use default props

--- src/buttons/icon.js
+++ src/buttons/icon.js
@@ -1,14 +1,17 @@
 import React from "react";
 import T from "prop-types";
 
-const Icon = ({ name, size = 26, format = name => `demo-icon icon-${name}` }) => (
-  <i className={format(name)} style={{ fontSize: size }} />
-);
+const Icon = ({ name, size, format }) => <i className={format(name)} style={{ fontSize: size }} />;
 
 Icon.propTypes = {
   format: T.func,
   name: T.string.isRequired,
   size: T.oneOfType([T.string, T.number]).isRequired,
+};
+
+Icon.defaultProps = {
+  format: name => `demo-icon icon-${name}`,
+  size: 26,
 };
 
 export default Icon;
