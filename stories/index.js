07ec86cb72fbce25bc37d516985222c8e89d7e48
Michal Szorád <miso.szorad@gmail.com>
2018-04-14 17:14:39 +0200

    Fix disable line

--- stories/index.js
+++ stories/index.js
@@ -4,5 +4,5 @@ import { storiesOf } from "@storybook/react";
 
 const renderAllButtons = buttons => <div>{Object.values(buttons).map((Button, i) => <Button key={i} />)}</div>;
 
-//eslint-ignore-next-line
+//eslint-disable-next-line
 storiesOf("Button", module).add("All buttons", () => renderAllButtons(buttons));
