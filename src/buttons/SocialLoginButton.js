817bf985f24cef9d29af8c75bcaaf180c7442150
Chan Chea Hang <cheahang@rydesharing.com>
2018-05-02 15:06:57 +0800

    Removed float left

--- src/buttons/SocialLoginButton.js
+++ src/buttons/SocialLoginButton.js
@@ -1,103 +1,179 @@
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+'use strict';
 
-import '../fontello-social/css/social-login-font.css';
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = undefined;
 
-const Icon = ({name, size = 26, format = (name) => `demo-icon icon-${name}`}) => <i className={format(name)}
-                                                                                    style={{fontSize: size}}/>;
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-export default class SocialLoginButton extends Component {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    static propTypes = {
+var _class, _temp;
 
-        /** Will be triggered when clicked on the button. */
-        onClick: PropTypes.func,
+var _propTypes = require('prop-types');
 
-        /** Custom button styles */
-        style: PropTypes.object,
+var _propTypes2 = _interopRequireDefault(_propTypes);
 
-        /** activeStyle styles will be applied instead of style when mouse hovers above the element */
-        activeStyle: PropTypes.object,
+var _react = require('react');
 
-        /** This text will be displayed */
-        text: PropTypes.string,
+var _react2 = _interopRequireDefault(_react);
 
-        /** This icon will be displayed */
-        icon: PropTypes.string,
+require('../fontello-social/css/social-login-font.css');
 
-        /** Box will have this size */
-        size: PropTypes.string,
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-        /** Icon will have this size. Eg. 26px */
-        iconSize: PropTypes.string,
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-        /** Format icon className. Eg. (name) => `fa-icon fa-icon-${name}` */
-        iconFormat: PropTypes.func,
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-        /** Text alignment of the button. Default 'left' */
-        textAlign: PropTypes.oneOf(['left', 'right', 'center'])
-    };
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    state = {hovered: false};
+var Icon = function Icon(_ref) {
+    var name = _ref.name,
+        _ref$size = _ref.size,
+        size = _ref$size === undefined ? 26 : _ref$size,
+        _ref$format = _ref.format,
+        format = _ref$format === undefined ? function (name) {
+            return 'demo-icon icon-' + name;
+        } : _ref$format;
+    return _react2.default.createElement('i', { className: format(name),
+        style: { fontSize: size } });
+};
 
-    constructor(...args) {
-        super(...args);
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-        this.handleMouseLeave = this.handleMouseLeave.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+var SocialLoginButton = (_temp = _class = function (_Component) {
+    _inherits(SocialLoginButton, _Component);
 
-    handleMouseEnter() {
-        this.setState({hovered: true});
-    }
+    function SocialLoginButton() {
+        var _ref2;
 
-    handleMouseLeave() {
-        this.setState({hovered: false});
-    }
+        _classCallCheck(this, SocialLoginButton);
 
-    handleClick() {
-        if (typeof this.props.onClick === 'function') {
-            this.props.onClick();
+        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
         }
+
+        var _this = _possibleConstructorReturn(this, (_ref2 = SocialLoginButton.__proto__ || Object.getPrototypeOf(SocialLoginButton)).call.apply(_ref2, [this].concat(args)));
+
+        _this.state = { hovered: false };
+
+        _this.handleMouseEnter = _this.handleMouseEnter.bind(_this);
+        _this.handleMouseLeave = _this.handleMouseLeave.bind(_this);
+        _this.handleClick = _this.handleClick.bind(_this);
+        return _this;
     }
 
-    render() {
-        const {style: customStyle, activeStyle, children, text = children, icon, iconFormat, iconSize = '26px', size = '50px', textAlign = 'left'} = this.props;
-        const {hovered} = this.state;
+    _createClass(SocialLoginButton, [{
+        key: 'handleMouseEnter',
+        value: function handleMouseEnter() {
+            this.setState({ hovered: true });
+        }
+    }, {
+        key: 'handleMouseLeave',
+        value: function handleMouseLeave() {
+            this.setState({ hovered: false });
+        }
+    }, {
+        key: 'handleClick',
+        value: function handleClick() {
+            if (typeof this.props.onClick === 'function') {
+                this.props.onClick();
+            }
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _props = this.props,
+                customStyle = _props.style,
+                activeStyle = _props.activeStyle,
+                children = _props.children,
+                _props$text = _props.text,
+                text = _props$text === undefined ? children : _props$text,
+                icon = _props.icon,
+                iconFormat = _props.iconFormat,
+                _props$iconSize = _props.iconSize,
+                iconSize = _props$iconSize === undefined ? '26px' : _props$iconSize,
+                _props$size = _props.size,
+                size = _props$size === undefined ? '50px' : _props$size,
+                _props$textAlign = _props.textAlign,
+                textAlign = _props$textAlign === undefined ? 'left' : _props$textAlign;
+            var hovered = this.state.hovered;
 
-        const buttonStyles = {
-            ...styles.button, ...{
+
+            var buttonStyles = _extends({}, styles.button, {
                 lineHeight: typeof text === 'string' ? size : 'auto',
                 height: size,
                 textAlign: textAlign
-            }, ...customStyle, ...(hovered && activeStyle)
-        };
+            }, customStyle, hovered && activeStyle);
 
-        const childrenCount = React.Children.count(children);
+            var childrenCount = _react2.default.Children.count(children);
 
-        // classic usage of this button
-        if (childrenCount === 0) {
-            return <div style={buttonStyles} onClick={this.handleClick} onMouseEnter={this.handleMouseEnter}
-                        onMouseLeave={this.handleMouseLeave}>
-                {icon && <span style={{...styles.icon, height: size, lineHeight: size}}>
-                    <Icon name={icon} size={iconSize} format={iconFormat}/>
-                </span>}
-                <span>{text}</span>
-            </div>
+            // classic usage of this button
+            if (childrenCount === 0) {
+                return _react2.default.createElement(
+                    'div',
+                    { style: buttonStyles, onClick: this.handleClick, onMouseEnter: this.handleMouseEnter,
+                        onMouseLeave: this.handleMouseLeave },
+                    icon && _react2.default.createElement(
+                    'span',
+                    { style: _extends({}, styles.icon, { height: size, lineHeight: size }) },
+                    _react2.default.createElement(Icon, { name: icon, size: iconSize, format: iconFormat })
+                    ),
+                    _react2.default.createElement(
+                        'span',
+                        null,
+                        text
+                    )
+                );
+            }
+
+            // children provided, rendering children as text
+            return _react2.default.createElement(
+                'div',
+                { style: buttonStyles, onClick: this.handleClick, onMouseEnter: this.handleMouseEnter,
+                    onMouseLeave: this.handleMouseLeave },
+                _react2.default.createElement('span', { style: styles.spanFix }),
+                text
+            );
         }
+    }]);
 
-        // children provided, rendering children as text
-        return <div style={buttonStyles} onClick={this.handleClick} onMouseEnter={this.handleMouseEnter}
-                    onMouseLeave={this.handleMouseLeave}>
-            <span style={styles.spanFix}/>
-            {text}
-        </div>
-    }
-}
+    return SocialLoginButton;
+}(_react.Component), _class.propTypes = {
 
-const styles = {
+    /** Will be triggered when clicked on the button. */
+    onClick: _propTypes2.default.func,
+
+    /** Custom button styles */
+    style: _propTypes2.default.object,
+
+    /** activeStyle styles will be applied instead of style when mouse hovers above the element */
+    activeStyle: _propTypes2.default.object,
+
+    /** This text will be displayed */
+    text: _propTypes2.default.string,
+
+    /** This icon will be displayed */
+    icon: _propTypes2.default.string,
+
+    /** Box will have this size */
+    size: _propTypes2.default.string,
+
+    /** Icon will have this size. Eg. 26px */
+    iconSize: _propTypes2.default.string,
+
+    /** Format icon className. Eg. (name) => `fa-icon fa-icon-${name}` */
+    iconFormat: _propTypes2.default.func,
+
+    /** Text alignment of the button. Default 'left' */
+    textAlign: _propTypes2.default.oneOf(['left', 'right', 'center'])
+}, _temp);
+exports.default = SocialLoginButton;
+
+
+var styles = {
     spanFix: {
-        height: '100%', display: 'inline-block', verticalAlign: 'middle',
+        height: '100%', display: 'inline-block', verticalAlign: 'middle'
     },
     button: {
         fontSize: '120%',
@@ -111,7 +187,6 @@ const styles = {
         padding: '0 10px'
     },
     icon: {
-        paddingRight: '10px',
-        float: 'left',
+        paddingRight: '20px'
     }
 };
