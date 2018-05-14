47084bf7a20e8b39d9a2fdcf674c262fedcb7e16
Michal Szorád <miso.szorad@gmail.com>
2018-05-14 19:22:05 +0200

    Use facebook svg

--- src/buttons/FacebookLoginButton.js
+++ src/buttons/FacebookLoginButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 import createButton from "./create-button";
+import createSvgIcon from "./createSvgIcon";
+import icon from "./facebook-logo.svg";
 
 const config = {
   activeStyle: { background: "#293e69" },
-  icon: "facebook-official",
+  icon: createSvgIcon(icon),
   style: { background: "#3b5998" },
   text: "Login with Facebook",
 };
