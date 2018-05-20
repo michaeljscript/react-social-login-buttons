cc701fa94c3eddf033ee8ae00f043e01c171c796
Michal Szorád <miso.szorad@gmail.com>
2018-05-20 09:32:45 +0200

    Add examples from readme

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,6 @@
 import * as buttons from "../src/buttons";
+import { FacebookLoginButton } from "../src/buttons";
+import createButton from "../src/buttons/create-button";
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
@@ -9,4 +11,21 @@ const renderAllButtons = buttons => (
 //eslint-disable-next-line
 storiesOf("Social login buttons", module)
   .addDecorator(story => <div style={{ fontFamily: "sans-serif" }}>{story()}</div>)
-  .add("All buttons", () => renderAllButtons(buttons));
+  .add("All buttons", () => renderAllButtons(buttons))
+  .add("With custom content", () => (
+    <FacebookLoginButton>
+      <span>Custom text</span>
+    </FacebookLoginButton>
+  ))
+  .add("Custom button", () => {
+    const config = {
+      text: "Login with Facebook",
+      icon: "facebook",
+      iconFormat: name => `fa fa-${name}`,
+      style: { background: "#3b5998" },
+      activeStyle: { background: "#293e69" },
+    };
+    /** My Facebook login button. */
+    const MyFacebookLoginButton = createButton(config);
+    return <MyFacebookLoginButton>Hello</MyFacebookLoginButton>;
+  });
