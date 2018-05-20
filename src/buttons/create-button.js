1e94d2e5291ecaa97e6aaa9c0259ece38e6c2219
Michal Szorád <miso.szorad@gmail.com>
2018-05-20 07:38:23 +0200

    Remove social login button provider

--- src/buttons/create-button.js
+++ src/buttons/create-button.js
@@ -1,7 +1,19 @@
 import React from "react";
+import SocialLoginButton from "./SocialLoginButton";
 import SocialLoginButtonProvider from "./SocialLoginButtonProvider";
 
 const createButton = config => props => {
+  const finalProps = {
+    // eslint-disable-next-line
+    activeStyle: { ...config.activeStyle, ...props.activeStyle },
+    ...config,
+    ...props,
+    // eslint-disable-next-line
+    style: { ...config.style, ...props.style },
+  };
+
+  return <SocialLoginButton {...finalProps} />;
+
   return <SocialLoginButtonProvider defaults={config} props={props} />;
 };
 
