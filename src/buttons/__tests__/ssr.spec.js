afd5455586ff495ad07c1c401eec60e96de36aa8
Michal Szorád <miso.szorad@gmail.com>
2018-06-07 13:44:27 +0200

    Add ssr tests

---
+++ src/buttons/__tests__/ssr.spec.js
@@ -0,0 +1,9 @@
+import * as buttons from "../index";
+import React from "react";
+import { renderToString } from "react-dom/server";
+
+describe("SSR", () => {
+  it("Should render without crash", () => {
+    Object.values(buttons).forEach((Button, i) => renderToString(<Button />));
+  });
+});
