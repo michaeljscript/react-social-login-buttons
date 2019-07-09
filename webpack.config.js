f63a1f257cdd790c55135b93614ff14cfd903162
Michal Szorád <miso.szorad@gmail.com>
2019-07-09 13:50:49 +0200

    Update dependencies

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2',
+      },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  };
