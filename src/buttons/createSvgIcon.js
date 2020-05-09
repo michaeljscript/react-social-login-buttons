6cc3ee98fd89e7528f20f918806062e2442ccd2a
Michal Szorád <miso.szorad@gmail.com>
2020-05-09 11:07:03 +0100

    Add support for icons with url

--- src/buttons/createSvgIcon.js
+++ src/buttons/createSvgIcon.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 // eslint-disable-next-line
-const createSvgIcon = Component => ({ size }) => <Component width={size} height={size} />;
+const createSvgIcon = Component => ({ size }) => {
+
+  // URL has been given
+  if (typeof Component === 'string') {
+    function Image (props) {
+      return <img src={Component} {...props} />;
+    }
+    return createSvgIcon(Image);
+  }
+  return <Component width={size} height={size} />;
+};
 
 export default createSvgIcon;
