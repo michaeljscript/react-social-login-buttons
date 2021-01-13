e0ee4aaeb1215e70b689392e7b683b39533e219e
Michal Szorád <miso.szorad@gmail.com>
2021-01-13 12:08:35 +0100

    Fix google logo

--- src/buttons/GoogleLoginButton.js
+++ src/buttons/GoogleLoginButton.js
@@ -14,7 +14,7 @@ const GoogleLoginButton = createButton(config);
 
 export default GoogleLoginButton;
 
-function Icon ({ width, height }) {
+function Icon({ width, height }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -22,32 +22,6 @@ function Icon ({ width, height }) {
       width={width}
       height={height}
     >
-      <radialGradient
-        id="95yY7w43Oj6n2vH63j6HJa"
-        cx="31.998"
-        cy="34.5"
-        r="30.776"
-        gradientTransform="matrix(1 0 0 -1 0 66)"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#f4e9c3"/>
-        <stop offset=".219" stopColor="#f8eecd"/>
-        <stop offset=".644" stopColor="#fdf4dc"/>
-        <stop offset="1" stopColor="#fff6e1"/>
-      </radialGradient>
-      <path
-        fill="url(#95yY7w43Oj6n2vH63j6HJa)"
-        d="M63.97,30.06C63.68,32.92,61.11,35,58.24,35H53c-1.22,0-2.18,1.08-1.97,2.34	c0.16,0.98,1.08,1.66,2.08,
-        1.66h3.39c2.63,0,4.75,2.28,4.48,4.96C60.74,46.3,58.64,48,56.29,48H51c-1.22,0-2.18,1.08-1.97,2.34
-        c0.16,0.98,1.08,1.66,2.08,1.66h3.39c1.24,0,2.37,0.5,3.18,1.32C58.5,54.13,59,55.26,59,56.5c0,2.49-2.01,4.5-4.5
-        ,4.5h-44	c-1.52,0-2.9-0.62-3.89-1.61C5.62,58.4,5,57.02,5,55.5c0-3.04,2.46-5.5,5.5-5.5H14c1.22,0,2.18-1.08,
-        1.97-2.34	C15.81,46.68,14.89,46,13.89,46H5.5c-2.63,0-4.75-2.28-4.48-4.96C1.26,38.7,3.36,37,5.71,37H13c1.71,
-        0,3.09-1.43,3-3.16	C15.91,32.22,14.45,31,12.83,31H4.5c-2.63,0-4.75-2.28-4.48-4.96C0.26,23.7,2.37,22,4.71,
-        22h9.79c1.24,0,2.37-0.5,3.18-1.32	C18.5,19.87,19,18.74,19,17.5c0-2.49-2.01-4.5-4.5-4.5h-6c-1.52,
-        0-2.9-0.62-3.89-1.61S3,9.02,3,7.5C3,4.46,5.46,2,8.5,2h48	c3.21,0,5.8,2.79,5.47,6.06C61.68,10.92,60.11
-        ,13,57.24,13H55.5c-3.04,0-5.5,2.46-5.5,5.5c0,1.52,0.62,2.9,1.61,3.89	C52.6,23.38,53.98,24,55.5,24h3C61.71
-        ,24,64.3,26.79,63.97,30.06z"
-      />
       <linearGradient
         id="95yY7w43Oj6n2vH63j6HJb"
         x1="29.401"
@@ -57,10 +31,10 @@ function Icon ({ width, height }) {
         gradientTransform="matrix(1 0 0 -1 0 66)"
         gradientUnits="userSpaceOnUse"
       >
-        <stop offset="0" stopColor="#ff5840"/>
-        <stop offset=".007" stopColor="#ff5840"/>
-        <stop offset=".989" stopColor="#fa528c"/>
-        <stop offset="1" stopColor="#fa528c"/>
+        <stop offset="0" stopColor="#ff5840" />
+        <stop offset=".007" stopColor="#ff5840" />
+        <stop offset=".989" stopColor="#fa528c" />
+        <stop offset="1" stopColor="#fa528c" />
       </linearGradient>
       <path
         fill="url(#95yY7w43Oj6n2vH63j6HJb)"
@@ -76,9 +50,9 @@ function Icon ({ width, height }) {
         gradientTransform="matrix(1 0 0 -1 0 66)"
         gradientUnits="userSpaceOnUse"
       >
-        <stop offset="0" stopColor="#feaa53"/>
-        <stop offset=".612" stopColor="#ffcd49"/>
-        <stop offset="1" stopColor="#ffde44"/>
+        <stop offset="0" stopColor="#feaa53" />
+        <stop offset=".612" stopColor="#ffcd49" />
+        <stop offset="1" stopColor="#ffde44" />
       </linearGradient>
       <path
         fill="url(#95yY7w43Oj6n2vH63j6HJc)"
@@ -94,9 +68,9 @@ function Icon ({ width, height }) {
         gradientTransform="matrix(1 0 0 -1 0 66)"
         gradientUnits="userSpaceOnUse"
       >
-        <stop offset="0" stopColor="#42d778"/>
-        <stop offset=".428" stopColor="#3dca76"/>
-        <stop offset="1" stopColor="#34b171"/>
+        <stop offset="0" stopColor="#42d778" />
+        <stop offset=".428" stopColor="#3dca76" />
+        <stop offset="1" stopColor="#34b171" />
       </linearGradient>
       <path
         fill="url(#95yY7w43Oj6n2vH63j6HJd)"
@@ -112,11 +86,11 @@ function Icon ({ width, height }) {
         gradientTransform="matrix(1 0 0 -1 0 66)"
         gradientUnits="userSpaceOnUse"
       >
-        <stop offset="0" stopColor="#155cde"/>
-        <stop offset=".278" stopColor="#1f7fe5"/>
-        <stop offset=".569" stopColor="#279ceb"/>
-        <stop offset=".82" stopColor="#2cafef"/>
-        <stop offset="1" stopColor="#2eb5f0"/>
+        <stop offset="0" stopColor="#155cde" />
+        <stop offset=".278" stopColor="#1f7fe5" />
+        <stop offset=".569" stopColor="#279ceb" />
+        <stop offset=".82" stopColor="#2cafef" />
+        <stop offset="1" stopColor="#2eb5f0" />
       </linearGradient>
       <path
         fill="url(#95yY7w43Oj6n2vH63j6HJe)"
