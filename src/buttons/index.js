58e231d193b107f3914b66137eaebd56dd15d8af
Antoine Jaussoin <antoine@jaussoin.com>
2021-07-03 16:14:31 +0100

    Addng Okta and Slack

--- src/buttons/index.js
+++ src/buttons/index.js
@@ -10,5 +10,7 @@ export { default as BufferLoginButton } from './BufferLoginButton';
 export { default as TelegramLoginButton } from './TelegramLoginButton';
 export { default as AppleLoginButton } from './AppleLoginButton';
 export { default as DiscordLoginButton } from './DiscordLoginButton';
+export { default as OktaLoginButton } from './OktaLoginButton';
+export { default as SlackLoginButton } from './SlackLoginButton';
 export { default as createButton } from './create-button';
 export { default as createSvgIcon } from './createSvgIcon';
