cad82d9c6239e9142c2cd94307664b7283d0086e
ApiDev234 <rockgaurishsethia@gmail.com>
2021-10-09 15:12:28 +0530

    Fixed

--- src/buttons/YahooLoginButton.js
+++ src/buttons/YahooLoginButton.js
@@ -12,7 +12,7 @@ const config = {
 
 const YahooLoginButton = createButton(config);
 
-export default TwitterLoginButton;
+export default YahooLoginButton;
 
 function Icon({ width, height, color }) {
   return <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
