e2a92245b44669e366f09885b2299f296ad8bb57
Michal <19684576+michaeljscript@users.noreply.github.com>
2025-02-27 09:22:26 +0100

    chore(react): Support React 19

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,23 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
